685d7bbdd9b160c2d51e794198af09a3cdbce78e
ThinkPad <ThinkPad@pc>
2020-03-28 11:09:29 +0800

    I8处理器

---
+++ com.awesometech.dps.processor.irobt/src/com/awesometech/dps/processor/irobt/Activator.java
@@ -0,0 +1,25 @@
+package com.awesometech.dps.processor.irobt;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends AbstractUIPlugin {
+	
+	private static Activator plugin;
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		plugin = this;
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+	}
+
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	
+}
